f6413f06f108dba5e9d286d544ca1e16ad6ecbae
psalv <psalvato@uwo.ca>
2017-03-10 23:58:22 -0500

    Moving through medium blog to better udnerstand routers.

--- app/App.js
+++ app/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
-const App = () => <h1>Hello World!</h1>
+class App extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path='/' component={Home} />
+                <Route path='/address' component={Address} />
+            </Router>
+        )
+    }
+}
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are on Bernard.</h1>
 
 export default App
